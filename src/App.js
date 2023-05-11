9ec5b3868b88f1a5cde63400ac1a2b59b61cf693
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-05-11 11:01:15 +0330

    Adding categories markup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 const App = () => {
-	return <div>Hello World</div>;
+	const categories = [
+		{id: "001", title: "Hats"},
+		{id: "002", title: "Jackets"},
+		{id: "003", title: "Sneakers"},
+		{id: "004", title: "Women"},
+		{id: "005", title: "Men"},
+	];
+	return (
+		<div className="categories-container">
+			{categories.map(category => {
+				return (
+					<div key={category.id} className="category-container">
+						{/*<img src="" alt="" />*/}
+						<div className="category-body-container">
+							<h2>{category.title}</h2>
+							<p>Shop Now</p>
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 
 export default App;
