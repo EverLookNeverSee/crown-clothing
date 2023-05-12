677275322234e174e41c11445647129cc387ecb7
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-05-12 12:05:33 +0330

    Routing home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import Directory from "./components/directory/directory.component";
-import "./components/directory/directory.styles.scss";
+import {Routes, Route} from "react-router-dom";
+import Home from "./routes/home/home.component";
 
 const App = () => {
-	const categories = [
-		{id: "001", title: "Hats", imageUrl: "https://i.ibb.co/cvpntL1/hats.png"},
-		{id: "002", title: "Jackets", imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"},
-		{id: "003", title: "Sneakers", imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"},
-		{id: "004", title: "Women", imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"},
-		{id: "005", title: "Men", imageUrl: "https://i.ibb.co/R70vBrQ/men.png"},
-	];
-	return <Directory categories={categories} />;
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+		</Routes>
+	);
 };
-
 export default App;
