8d13353981c70477b259744d069f53b224ddf221
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-05-12 12:05:17 +0330

    Implementing home component

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,14 @@
+import Directory from "../../components/directory/directory.component";
+
+const Home = () => {
+	const categories = [
+		{id: "001", title: "Hats", imageUrl: "https://i.ibb.co/cvpntL1/hats.png"},
+		{id: "002", title: "Jackets", imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"},
+		{id: "003", title: "Sneakers", imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"},
+		{id: "004", title: "Women", imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"},
+		{id: "005", title: "Men", imageUrl: "https://i.ibb.co/R70vBrQ/men.png"},
+	];
+	return <Directory categories={categories} />;
+};
+
+export default Home;
