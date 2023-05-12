b231a2f9bc1894cf69a14b1794df5c3a4a310e69
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-05-12 10:51:21 +0330

    App component refactor

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import "./categories.styles.scss";
+import Directory from "./components/directory/directory.component";
+import "./components/directory/directory.styles.scss";
 
 const App = () => {
 	const categories = [
-		{id: "001", title: "Hats"},
-		{id: "002", title: "Jackets"},
-		{id: "003", title: "Sneakers"},
-		{id: "004", title: "Women"},
-		{id: "005", title: "Men"},
+		{id: "001", title: "Hats", imageUrl: "https://i.ibb.co/cvpntL1/hats.png"},
+		{id: "002", title: "Jackets", imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"},
+		{id: "003", title: "Sneakers", imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"},
+		{id: "004", title: "Women", imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"},
+		{id: "005", title: "Men", imageUrl: "https://i.ibb.co/R70vBrQ/men.png"},
 	];
-	return (
-		<div className="categories-container">
-			{categories.map(category => {
-				return (
-					<div key={category.id} className="category-container">
-						{/*<img src="" alt="" />*/}
-						<div className="category-body-container">
-							<h2>{category.title}</h2>
-							<p>Shop Now</p>
-						</div>
-					</div>
-				);
-			})}
-		</div>
-	);
+	return <Directory categories={categories} />;
 };
 
 export default App;
