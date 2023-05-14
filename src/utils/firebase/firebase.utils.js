c2d2b84fda3e380f74a65895e6aa4ca51e594bb7
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-05-14 11:31:54 +0330

    Implementing sign in with popup functionality

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import {initializeApp} from "firebase/app";
 import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+import {getFirestore, doc, getDoc, setDoc} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,3 +19,18 @@ provider.setCustomParameters({prompt: "select_account"});
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async userAuth => {
+	const userDocRef = doc(db, "users", userAuth.uid);
+	const userSnapshot = await getDoc(userDocRef);
+	if (!userSnapshot.exists()) {
+		const {displayName, email} = userAuth;
+		const createdAt = new Date();
+		try {
+			await setDoc(userDocRef, {displayName, email, createdAt});
+		} catch (err) {
+			console.log(`Error in creating the user: ${err.message}`);
+		}
+	}
+	return userDocRef;
+};
