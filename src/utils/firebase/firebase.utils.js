c327b66c02fbff0e99e6de3041be6c3aa45ede71
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-05-16 11:19:20 +0330

    Importing relevant modules

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,10 @@
 import {initializeApp} from "firebase/app";
-import {getAuth, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+import {
+	getAuth,
+	signInWithPopup,
+	GoogleAuthProvider,
+	createUserWithEmailAndPassword,
+} from "firebase/auth";
 import {getFirestore, doc, getDoc, setDoc} from "firebase/firestore";
 
 // Your web app's Firebase configuration
