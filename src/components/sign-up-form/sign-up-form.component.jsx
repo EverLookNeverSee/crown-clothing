1f0b667889c16e1c3df148570c6c54f86a8791b9
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-05-16 13:33:28 +0330

    Styling sign-up form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -4,6 +4,7 @@ import {
 	createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
+import "./sign-up-form.styles.scss";
 
 const defaultFormFields = {
 	displayName: "",
@@ -41,8 +42,9 @@ const SignUpForm = () => {
 		setFormFields({...formFields, [name]: value});
 	};
 	return (
-		<div>
-			<h1>Sign up with your email and password</h1>
+		<div className="sign-up-container">
+			<h2>Don't have an account?</h2>
+			<span>Sign up with your email and password</span>
 			<form onSubmit={handleSubmit}>
 				<FormInput
 					label="Display Name"
