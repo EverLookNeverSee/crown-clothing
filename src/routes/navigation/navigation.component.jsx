8e7188bb2f24be1e6acf3895b538c640cb5a028f
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-05-18 20:58:24 +0330

    Styling navigation component based on user context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,16 @@
 import {Outlet, Link} from "react-router-dom";
-import {Fragment} from "react";
+import {Fragment, useContext} from "react";
 import {ReactComponent as CrownLogo} from "../../assets/007 crown[UdemyIran.Com].svg";
 import "./navigation.styles.scss";
+import {UserContext} from "../../context/user.context";
+import {signOutUser} from "../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
+	const {currentUser, setCurrentUser} = useContext(UserContext);
+	const signOutHandler = async () => {
+		const res = await signOutUser();
+		setCurrentUser(null);
+	};
 	return (
 		<Fragment>
 			<div className="navigation">
@@ -14,9 +21,15 @@ const Navigation = () => {
 					<Link className="nav-link" to="/shop">
 						Shop
 					</Link>
-					<Link to="/auth" className="nav-link">
-						Sign In
-					</Link>
+					{currentUser ? (
+						<span className="nav-link" onClick={signOutHandler}>
+							Sign Out
+						</span>
+					) : (
+						<Link to="/auth" className="nav-link">
+							Sign In
+						</Link>
+					)}
 				</div>
 			</div>
 			<Outlet />
