ff17efa68e97ab240d1c71793301d953da2c67db
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-05-21 19:16:17 +0330

    Centralizing auth state changes tracking

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,11 +6,8 @@ import {UserContext} from "../../context/user.context";
 import {signOutUser} from "../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
-	const {currentUser, setCurrentUser} = useContext(UserContext);
-	const signOutHandler = async () => {
-		const res = await signOutUser();
-		setCurrentUser(null);
-	};
+	const {currentUser} = useContext(UserContext);
+
 	return (
 		<Fragment>
 			<div className="navigation">
@@ -22,7 +19,7 @@ const Navigation = () => {
 						Shop
 					</Link>
 					{currentUser ? (
-						<span className="nav-link" onClick={signOutHandler}>
+						<span className="nav-link" onClick={signOutUser}>
 							Sign Out
 						</span>
 					) : (
