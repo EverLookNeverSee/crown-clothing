349e11de0dd6e592a94ba8b77a8f0fde6ff6b468
EverLookNeverSee <EverLookNeverSee@Proton.Me>
2023-05-26 21:24:53 +0330

    Using CartContext inside navigation component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,12 +3,14 @@ import {Fragment, useContext} from "react";
 import {ReactComponent as CrownLogo} from "../../assets/007 crown[UdemyIran.Com].svg";
 import "./navigation.styles.scss";
 import {UserContext} from "../../context/user.context";
+import {CartContext} from "../../context/cart.context";
 import {signOutUser} from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
 	const {currentUser} = useContext(UserContext);
+	const {isCartOpen} = useContext(CartContext);
 
 	return (
 		<Fragment>
@@ -31,7 +33,7 @@ const Navigation = () => {
 					)}
 					<CartIcon />
 				</div>
-				<CartDropdown />
+				{isCartOpen && <CartDropdown />}
 			</div>
 			<Outlet />
 		</Fragment>
